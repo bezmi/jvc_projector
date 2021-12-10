09dc0dba2066c942bdb311903abbc12c87313aa3
Shahzeb Imran <s.imran@tuta.io>
2021-12-10 11:54:24 +1000

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jvc_projector_remote",
-    version="0.0.4",
+    version="0.1.0",
     author="bezmi",
     description="A small package to control jvc projectors over IP",
     long_description=long_description,
