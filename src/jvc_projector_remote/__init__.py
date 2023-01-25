c2baa0923c14a5126ee790b1784a262359ff519b
Shahzeb Imran <s.imran@tuta.io>
2023-01-25 13:16:18 +1000

    print version on import when debug enabled

--- src/jvc_projector_remote/__init__.py
+++ src/jvc_projector_remote/__init__.py
@@ -1,5 +1,8 @@
 from .jvcprojector import *
 from .jvccommands import *
 from importlib.metadata import version
+import logging
 
 __version__ = version('jvc_projector_remote')
+logging.getLogger(__name__).debug(f"imported jvc_projector_remote {__version__}")
+
